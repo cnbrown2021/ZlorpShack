5f8b8c88928c33aee6d063f168a1036caf238e8d
Ciara Brown <cnbrown2021@gmail.com>
2021-10-26 15:23:34 -0400

    Student class

---
+++ ZlorpShack.Data/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZlorpShack.Data
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public int Grade { get; set; }
+        public int NumberOfBooksRead { get; set; }
+        public int CurrentRewardTier { get; set; }
+        public virtual string ListOfBooks { get; set; }
+    }
+}
