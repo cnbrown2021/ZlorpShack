efbba406252ac99ab94d7fbbdb5f41297c123aca
Jesse Southerland <jessemsoutherland@gmail.com>
2021-10-27 16:44:59 -0400

    Controller added

---
+++ ZlorpShack.WebAPI/Controllers/AwardEarnedController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZlorpShack.Service;
+
+namespace ZlorpShack.WebAPI.Controllers
+{
+    public class AwardEarnedController : ApiController
+    {
+        private AwardEarnedService CreateAEService()
+        {
+            var aeService = new AwardEarnedService();
+            return aeService;
+        }
+
+        public H
+    }
+}
