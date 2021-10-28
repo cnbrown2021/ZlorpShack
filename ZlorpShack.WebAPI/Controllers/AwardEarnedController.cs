af0f70f15148035bacb96f616442759f0d1e4e39
Jesse Southerland <jessemsoutherland@gmail.com>
2021-10-28 11:02:16 -0400

    AE Controller

--- ZlorpShack.WebAPI/Controllers/AwardEarnedController.cs
+++ ZlorpShack.WebAPI/Controllers/AwardEarnedController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ZlorpShack.Model;
 using ZlorpShack.Service;
 
 namespace ZlorpShack.WebAPI.Controllers
@@ -14,6 +15,62 @@ namespace ZlorpShack.WebAPI.Controllers
         {
             var aeService = new AwardEarnedService();
             return aeService;
+        }
+
+        public IHttpActionResult Post(AwardEarnedCreate ae)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateAEService();
+
+            if (!service.CreateEarnedAward(ae))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+        public IHttpActionResult Get()
+        {
+            var service = CreateAEService();
+
+            var ae = service.GetAllAwardsEarned();
+
+            return Ok(ae);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var service = CreateAEService();
+
+            var ae = service.GetAwardByID(id);
+
+            return Ok(ae);
+        }
+
+        public IHttpActionResult Put(AwardEarnedEdit ae)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateAEService();
+
+            if (!service.UpdateAwardEarned(ae))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateAEService();
+
+            if (!service.DeleteAwardEarned(id))
+                return InternalServerError();
+
+            return Ok();
         }
     }
 }
